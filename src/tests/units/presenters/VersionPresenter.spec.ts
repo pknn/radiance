e59253eb381c84efa635e3177d5ceb1124b2764a
Pakanon Pantisawat <pakanon.pk@gmail.com>
2021-01-25 19:53:25 +0700

    :truck: renamed Version -> VersionPresenter

---
+++ src/tests/units/presenters/VersionPresenter.spec.ts
@@ -0,0 +1,20 @@
+import { expectCt } from "helmet";
+import { version } from "os";
+import { VersionPresenter } from "../../../presenters/Version";
+
+let versionPresenter: VersionPresenter;
+
+beforeAll(() => {
+  versionPresenter = {
+    version_number: "1.0.0",
+  };
+});
+
+describe("VersionPresenter", () => {
+  describe("presenter properties", () => {
+    it("should has correct property", () => {
+      expect(versionPresenter).toHaveProperty("version_number");
+      expect(versionPresenter.version_number).toBe("1.0.0");
+    });
+  });
+});
