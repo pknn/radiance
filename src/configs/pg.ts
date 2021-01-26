d8447dc8036b6c3cea638a8e848e08741da03df8
Pakanon Pantisawat <pakanon.pk@gmail.com>
2021-01-26 15:30:06 +0700

    edited database connection configuration to use test database for testing

--- src/configs/pg.ts
+++ src/configs/pg.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import consola from "consola";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
 });
 
 pool.on("error", (err) => consola.error(err));
